69274e60a81da2ad53f71f9be8b6e82b5c94438e
Suellen Lopes Gualberto <suellenlopesgualberto@gmail.com>
2023-06-16 10:36:05 -0300

    Component Link and reuse page

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,22 @@
+import PaginaPadrao from 'components/PaginaPadrao';
 import Menu from 'components/menu';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <Router>
-      < Menu/>
-      <Routes>
-        <Route  path='/' element={<Inicio />}/>
-        <Route  path='/cardapio' element={<Cardapio />}/>
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        < Menu />
+
+        <Routes>
+          <Route path='/' element={<PaginaPadrao />} >
+            <Route index element={<Inicio />} />
+            <Route path='cardapio' element={<Cardapio />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
